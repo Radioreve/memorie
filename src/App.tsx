599804e32527b22c280c0b716aa4853602aa1869
LJA <leo.jacquemin@octo.com>
2023-12-08 11:38:36 +0100

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Memori from "./Memori";
+
+function App() {
+  return (
+    <Memori
+      symbols={["💅", "🥺", "🔥", "✨", "💣", "👏", "🍰", "😇", "a", "b", "c"]}
+    />
+  );
+}
+
+export default App;
---
+++ src/Memori.tsx
@@ -0,0 +1,121 @@
+import { FunctionComponent, useState } from "react";
+import _ from "lodash";
+
+type MemoriItemType = {
+  id: number;
+  symbol: any;
+  state: "hidden" | "flipped" | "found";
+};
+
+interface MemoriItemProps {
+  symbol: number;
+  onClick: (number: number) => void;
+}
+
+const MemoriItem: FunctionComponent<MemoriItemProps> = ({
+  symbol,
+  onClick,
+}) => <div onClick={() => onClick(symbol)}></div>;
+
+export default function Memori({ symbols }: { symbols: string[] }) {
+  const init = _(symbols)
+    .map((s): Omit<MemoriItemType, "id"> => ({ state: "hidden", symbol: s }))
+    .flatMap((s) => [s, s])
+    .map((s, i) => ({ ...s, id: i }))
+    .shuffle()
+    .value();
+
+  console.log(init);
+
+  const [memoryItems, setMemoryItems] = useState<MemoriItemType[]>(init);
+
+  const handleClicked = (memoryItem: MemoriItemType) => {
+    const memoryItemSelected = memoryItems.find(
+      (itm) => itm.id === memoryItem.id,
+    ) as MemoriItemType;
+
+    if (memoryItemSelected.state !== "hidden") {
+      return console.log("Already flipped or found, doing nothing...");
+    }
+
+    const updatedMemoryItems = [...memoryItems];
+    memoryItemSelected.state = "flipped";
+    const index = _.findIndex(memoryItems, (itm) => itm.id === memoryItem.id);
+    updatedMemoryItems[index] = memoryItemSelected;
+
+    const flippedItems = updatedMemoryItems.filter(
+      (itm) => itm.state === "flipped",
+    );
+
+    if (flippedItems.length === 1) {
+      console.log("Flipped one item, waiting for second one...");
+      return setMemoryItems(updatedMemoryItems);
+    }
+
+    setMemoryItems(updatedMemoryItems);
+
+    if (
+      flippedItems.length === 2 &&
+      flippedItems[0].symbol === flippedItems[1].symbol
+    ) {
+      flippedItems[0].state = "found";
+      const index1 = _.findIndex(
+        memoryItems,
+        (itm) => itm.id === flippedItems[0].id,
+      );
+      updatedMemoryItems[index1] = flippedItems[0];
+      flippedItems[1].state = "found";
+      const index2 = _.findIndex(
+        memoryItems,
+        (itm) => itm.id === flippedItems[1].id,
+      );
+      updatedMemoryItems[index2] = flippedItems[1];
+      console.log("Found a match!");
+      return setMemoryItems(updatedMemoryItems);
+    }
+
+    setTimeout(() => {
+      console.log("Flipping back the two...");
+      const updatedMemoryItems = [...memoryItems];
+      flippedItems[0].state = "hidden";
+      const index1 = _.findIndex(
+        memoryItems,
+        (itm) => itm.id === flippedItems[0].id,
+      );
+      updatedMemoryItems[index1] = flippedItems[0];
+      flippedItems[1].state = "hidden";
+      const index2 = _.findIndex(
+        memoryItems,
+        (itm) => itm.id === flippedItems[1].id,
+      );
+      updatedMemoryItems[index2] = flippedItems[1];
+      setMemoryItems(updatedMemoryItems);
+    }, 1000);
+  };
+
+  return (
+    <ul className="memori">
+      {memoryItems.map((memoryItem, i) => {
+        if (memoryItem.state === "hidden") {
+          return (
+            <li key={memoryItem.id}>
+              <MemoriItem
+                symbol={memoryItem.symbol}
+                onClick={() => {
+                  handleClicked(memoryItem);
+                }}
+              />
+            </li>
+          );
+        }
+        if (memoryItem.state === "flipped" || memoryItem.state === "found") {
+          return (
+            <li className="flipped" key={memoryItem.id}>
+              {memoryItem.symbol}
+            </li>
+          );
+        }
+      })}
+    </ul>
+  );
+}
