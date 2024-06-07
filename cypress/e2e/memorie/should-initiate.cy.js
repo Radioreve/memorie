c95717725158c2c6385e15f9885500f59739c5e6
LJA <leo.jacquemin@octo.com>
2024-06-07 16:33:04 +0200

    add empty test cases

--- cypress/e2e/memorie/should-initiate.cy.js
+++ cypress/e2e/memorie/should-initiate.cy.js
@@ -6,4 +6,10 @@ describe("example to-do app", () => {
   it("should display my app", () => {
     cy.get("li").should("have.length", 16);
   });
+
+  it("should flip a card if it is hidden", () => {});
+  it("should not flip a hidden during the memorization phase", () => {});
+  it("should not hide a flipped card until another hidden card is flipped", {});
+  it("should let a memorization time when 2 flipped cards are different", () => {});
+  it("should let 2 identifical flipped cards, remain permanently flipped", () => {});
 });
